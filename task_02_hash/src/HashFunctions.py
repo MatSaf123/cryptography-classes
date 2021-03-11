90ac4f8014b7c10f7a8872a7ec54c34be11e94c9
MatSaf123 <xnerov13@gmail.com>
2021-03-11 19:58:03 +0100

    created Hashing class for hash functions

--- task_02_hash/src/HashFunctions.py
+++ task_02_hash/src/HashFunctions.py
@@ -5,58 +5,62 @@ import random
 import string
 import plotly.express as px
 
-logger = logging.getLogger('hashing')
-logging.basicConfig(level=logging.DEBUG)
 
+class Hashing:
 
-def hash_with_all(str_in: str, display=True) -> dict:
-    d_out = {}
+    logger = logging.getLogger('hashing')
+    logging.basicConfig(level=logging.DEBUG)
 
-    for fun in hl.algorithms_available:
+    @staticmethod
+    def hash_with_all(str_in: str, display=True) -> dict:
+        d_out = {}
 
-        out = hl.new(fun, str_in.encode("UTF-8"))
-        t = timeit.timeit('lambda: hl.new(fun, str_in.encode("UTF-8"))')
-        h = None
-        if fun.startswith('shake'):
-            h = str(out.hexdigest(20))
-        else:
-            h = str(out.hexdigest())
-        if display:
-            logger.info(fun + '(): ' + h + ' , (t = ' + str(t) + ')')
-        d_out[fun] = h
+        for fun in hl.algorithms_available:
 
-    return d_out
-
-
-def hash_from_file(file_path: str, display=True) -> str:
-    try:
-        with open(file_path, 'rb') as file:
-            out = hl.sha256()
-            while True:
-                chunk = file.read(out.block_size)
-                if not chunk:
-                    break
-                out.update(chunk)
+            out = hl.new(fun, str_in.encode("UTF-8"))
+            t = timeit.timeit('lambda: hl.new(fun, str_in.encode("UTF-8"))')
+            h = None
+            if fun.startswith('shake'):
+                h = str(out.hexdigest(20))
+            else:
+                h = str(out.hexdigest())
             if display:
-                logger.info(out.hexdigest())
-        return out.hexdigest()
-    except FileNotFoundError:
-        raise FileNotFoundError('There is no file within given path')
+                Hashing.logger.info(fun + '(): ' + h + ' , (t = ' + str(t) + ')')
+            d_out[fun] = h
 
+        return d_out
 
-def present_hash_time_for_strings(r: int = 30, algorithm: str = 'md5'):
-    results = {
-        'string_lengths': [],
-        'hash_speed': []
-    }
+    @staticmethod
+    def hash_from_file(file_path: str, display=True) -> str:
+        try:
+            with open(file_path, 'rb') as file:
+                out = hl.sha256()
+                while True:
+                    chunk = file.read(out.block_size)
+                    if not chunk:
+                        break
+                    out.update(chunk)
+                if display:
+                    Hashing.logger.info(out.hexdigest())
+            return out.hexdigest()
+        except FileNotFoundError:
+            raise FileNotFoundError('There is no file within given path')
 
-    for i in range(1, r + 1):
-        s = ''.join(random.choices(string.ascii_lowercase + string.digits, k=i))
-        results['string_lengths'].append(i)
-        results['hash_speed'].append(timeit.timeit('lambda: hl.new(fun, s.encode("UTF-8"))'))
+    @staticmethod
+    def present_hash_time_for_strings(r: int = 30, algorithm: str = 'md5'):
+        results = {
+            'string_lengths': [],
+            'hash_speed': []
+        }
 
-    fig = px.line(results, x='string_lengths', y='hash_speed')
-    fig.show()
+        for i in range(1, r + 1):
+            s = ''.join(random.choices(string.ascii_lowercase + string.digits, k=i))
+            results['string_lengths'].append(i)
+            results['hash_speed'].append(timeit.timeit('lambda: hl.new(fun, s.encode("UTF-8"))'))
 
-# present_hash_time_for_strings()
-# present_hash_time_for_strings(algorithm='shake_128')
+        fig = px.line(results, x='string_lengths', y='hash_speed')
+        fig.show()
+
+# Hashing.present_hash_time_for_strings()
+# Hashing.present_hash_time_for_strings(algorithm='shake_128')
+# Hashing.hash_with_all('hello')
