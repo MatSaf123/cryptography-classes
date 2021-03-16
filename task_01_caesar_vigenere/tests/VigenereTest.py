d4b882b17e9e45232d9c88004f911f686a6e6f72
MatSaf123 <xnerov13@gmail.com>
2021-03-16 19:20:06 +0100

    added alias for class name

--- task_01_caesar_vigenere/tests/VigenereTest.py
+++ task_01_caesar_vigenere/tests/VigenereTest.py
@@ -1,43 +1,43 @@
 import pytest
-from task_01_caesar_vigenere.src.Vigenere import VigenereCoder
+from task_01_caesar_vigenere.src.Vigenere import VigenereCoder as vc
 
 
-#   ENCODER
+#   ENCODE
 
 def test_encode():
     test_str = 'ABCDXYZ'
-    assert VigenereCoder.encode(test_str, 'TEST') == 'TFUWQCR'
+    assert vc.encode(test_str, 'TEST') == 'TFUWQCR'
 
 
 def test_encode_long():
     #   https://www.lipsum.com
     test_str = 'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT MAURIS AUGUE NISI SOLLICITUDIN ID SAGITTIS SED TRISTIQUE VEL IPSUM PROIN ALIQUAM LUCTUS MAURIS'
-    assert VigenereCoder.encode(test_str,
+    assert vc.encode(test_str,
                                 'TEST') == 'ESJXF MHLNQ VHESJ LBX SFXX UHGWWVMILNK EVBIMKVBRY XEML FTYJBL EMZNI FBLM KHEPAVBXMWBR AW LEYBMXAL LIV MKMKMBUMX OID BIWMF IVGBG EDBJYSF EYUMNW ETNVAL'
 
 
 def test_encode_invalid_char():
     test_str = 'ab_34_$%'
-    assert VigenereCoder.encode(test_str, 'TEST') == 'ab_34_$%'
+    assert vc.encode(test_str, 'TEST') == 'ab_34_$%'
 
 
-#   DECODER
+#   DECODE
 
 def test_decode():
     test_str = 'TFUWQCR'
-    assert VigenereCoder.decode(test_str, 'TEST') == 'ABCDXYZ'
+    assert vc.decode(test_str, 'TEST') == 'ABCDXYZ'
 
 
 def test_decode_long():
     #   https://www.lipsum.com
     test_str = 'ESJXF MHLNQ VHESJ LBX SFXX UHGWWVMILNK EVBIMKVBRY XEML FTYJBL EMZNI FBLM KHEPAVBXMWBR AW LEYBMXAL LIV MKMKMBUMX OID BIWMF IVGBG EDBJYSF EYUMNW ETNVAL'
-    assert VigenereCoder.decode(test_str,
+    assert vc.decode(test_str,
                                 'TEST') == 'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT MAURIS AUGUE NISI SOLLICITUDIN ID SAGITTIS SED TRISTIQUE VEL IPSUM PROIN ALIQUAM LUCTUS MAURIS'
 
 
 def test_decode_invalid_char():
     test_str = 'ab_34_$%'
-    assert VigenereCoder.decode(test_str, 'TEST') == 'ab_34_$%'
+    assert vc.decode(test_str, 'TEST') == 'ab_34_$%'
 
 
 #   ---
@@ -45,18 +45,18 @@ def test_decode_invalid_char():
 def test_empty_str_in():
     with pytest.raises(AssertionError):
         test_str = ''
-        VigenereCoder.encode(test_str, 'TEST')
+        vc.encode(test_str, 'TEST')
 
 
 def test_cleartext_with_spaces():
     test_str = 'ABCD EFG H'
-    assert VigenereCoder.encode(test_str, 'TEST') == 'TFUW XJY A'
+    assert vc.encode(test_str, 'TEST') == 'TFUW XJY A'
 
 
 def test_generating_key():
-    assert VigenereCoder.generate_key_from_secret('TEST ONE TWO THREE', 'TEST') == 'TEST TES TTE STTES'
+    assert vc.generate_key_from_secret('TEST ONE TWO THREE', 'TEST') == 'TEST TES TTE STTES'
 
 
 def test_invalid_secret():
     with pytest.raises(AssertionError):
-        result = VigenereCoder.decode('somecleartext', '__invalid_secret__')
+        vc.decode('somecleartext', '__invalid_secret__')
