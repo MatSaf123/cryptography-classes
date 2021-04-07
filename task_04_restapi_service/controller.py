7801b35ec7530f5f25d22a0f6b594d1c51ff1586
MatSaf123 <xnerov13@gmail.com>
2021-04-07 21:07:03 +0200

    Add all symmetric functionality

--- task_04_restapi_service/controller.py
+++ task_04_restapi_service/controller.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from encrypting.encrypt_symmetric import SymetricEncrypter
+from util import is_hexadecimal
 import logging
 
 logger = logging.getLogger('controller')
@@ -14,9 +15,10 @@ SE = SymetricEncrypter()
 
 @app.get('/symmetric/key')
 def get_random_key_symmetric() -> str:
-    """Generate and return randomly generated symmetric key
+    """Generates and returns a random symmetric key
 
     :return: randomly generated symmetric key
+    :rtype: str (hexadecimal)
     """
 
     key = SE.generate_random_key()
@@ -25,21 +27,49 @@ def get_random_key_symmetric() -> str:
 
 @app.post('/symmetric/key')
 def set_key_symmetric(key: str) -> bool:
-    """Set the symmetric key on the server
+    """Sets up symmetric key on server
 
-    :param key: key provided in hexadecimal format
-    :return:
+    :param key: key in hexadecimal format provided by user
+    :return: True if successfully saved the key, False if didn't
+    :rtype: bool
     """
-    try:
-        int(key, 16)    # raise ValueError if cannot convert from hex to int
+
+    if is_hexadecimal(key):
         SE.KEY = key
-    except ValueError:
-        logger.info('Provided key value is not in hexadecimal format.')
-        return False
-    except Exception:
-        logger.info('There was an error, try to enter different key value.')
-        return False
-    else:
         return True
+    else:
+        return False
 
 
+@app.post('/symmetric/encode')
+def symmetric_encode(message: str) -> bytes:
+    """Encodes message entered by user with symmetrical key set on server
+
+    :param message: cleartext to be encoded
+    :return: encoded message
+    :rtype: bytes
+    """
+
+    try:
+        assert SE.KEY is not None, 'Key value is not set'
+    except AssertionError:
+        logger.info('Set the key on the server before encoding a message.')
+    else:
+        return SE.encode(message)
+
+
+@app.post('/symmetric/decode')
+def symmetric_decode(message: str) -> bytes:
+    """Decodes message entered by user with symmetrical key set on server
+
+    :param message: encoded text to be decoded
+    :return: decoded message
+    :rtype: bytes
+    """
+
+    try:
+        assert SE.KEY is not None
+    except AssertionError:
+        logger.info('Set the key on the server before decoding a message.')
+    else:
+        return SE.decode(message)
--- task_04_restapi_service/encrypting/encrypt_symmetric.py
+++ task_04_restapi_service/encrypting/encrypt_symmetric.py
@@ -1,17 +1,39 @@
-import os
 from cryptography.fernet import Fernet
 
 
 class SymetricEncrypter:
 
-    KEY: str
+    def __init__(self):
+        self.KEY = None
 
-    @staticmethod
-    def generate_random_key() -> hex:
-        """Symmetrically generate a random key
+    def generate_random_key(self) -> str:
+        """Generate a random key
 
         :return: randomly generated key
+        :rtype: str (hexadecimal)
         """
 
         key = Fernet.generate_key()
         return key.hex()
+
+    def encode(self, message: str) -> bytes:
+        """Symmetrically encrypts cleartext
+
+        :param message: cleartext entered by user
+        :return: encrypted message
+        :rtype: bytes
+        """
+
+        f = Fernet(bytes.fromhex(self.KEY))
+        return f.encrypt(bytes(message, 'utf-8'))
+
+    def decode(self, message) -> bytes:
+        """Symmetrically decrypts encrypted text
+
+        :param message: encrypted text entered by user
+        :return: decrypted message
+        :rtype: bytes
+        """
+
+        f = Fernet(bytes.fromhex(self.KEY))
+        return f.decrypt(bytes(message, 'utf-8'))
