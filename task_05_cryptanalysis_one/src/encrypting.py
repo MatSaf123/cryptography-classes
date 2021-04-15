6c13d0b8794c34f8dc04bd5f4e1b8bb98192ad37
MatSaf123 <xnerov13@gmail.com>
2021-04-16 00:14:36 +0200

    Some more cleaning

--- task_05_cryptanalysis_one/src/encrypting.py
+++ task_05_cryptanalysis_one/src/encrypting.py
@@ -21,7 +21,7 @@ class Encrypter:
         elif not cleartext.isalpha():
             raise ValueError('Cleartext contains not allowed characters (allowed chars: a-z A-Z)')
 
-    def encrypt(self, cleartext: str) -> str:
+    def encrypt(self, cleartext: str, format_text: bool = False) -> str:
         """Encrypts cleartext with custom cipher
 
         :param cleartext: text about to be encrypted
@@ -31,12 +31,21 @@ class Encrypter:
         # converts cleartext to uppercase, removes whitespaces
         cleartext = cleartext.upper().replace(' ', '')
 
-
         try:
             self.validate_cleartext(cleartext)
         except ValueError as e:
-            self.logger.info(str(e))
-            return ''
+
+            if format_text:
+                formatted_cleartext = []
+                for c in cleartext:
+                    if c.isalpha():
+                        formatted_cleartext.append(c)
+                    else:
+                        continue
+                cleartext = ''.join(formatted_cleartext)
+            else:
+                self.logger.info(str(e))
+                return ''
 
         encrypted_chars: list = []
 
@@ -62,7 +71,7 @@ class Encrypter:
         for i in range(len(cipher)):
             c = cipher[i]
 
-            stride = int((len(cipher) - i) % 26)
+            stride = int((len(cipher) - i) % self.__ALPHABET_SIZE)
 
             decrypted_chars.append(chr((ord(
                 c) - stride - self.__UPPERCASE_A_ASCII_CODE) % self.__ALPHABET_SIZE + self.__UPPERCASE_A_ASCII_CODE))
