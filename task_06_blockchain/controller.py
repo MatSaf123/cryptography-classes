1196b4a811f85421ed7eabe435a5d2ecd70c6300
MatSaf123 <xnerov13@gmail.com>
2021-05-07 01:04:42 +0200

    More work on blockchain, UI

--- task_06_blockchain/controller.py
+++ task_06_blockchain/controller.py
@@ -18,6 +18,8 @@ blockchain = Blockchain()
 
 @app.get('/chain', response_class=HTMLResponse)
 def chain(request: Request):
+    """Get blockchain route
+    """
 
     chain_data = []
     for block in blockchain.chain:
@@ -31,6 +33,12 @@ def chain(request: Request):
 
 @app.post('/new_transaction', response_class=HTMLResponse)
 def new_transaction(request: Request, sender: str = Form(...), recipient: str = Form(...), amount: str = Form(...)):
+    """New transaction post
+
+    :param sender: Person sending the money
+    :param recipient: Person getting the money
+    :param amount: Amount of money sent
+    """
 
     transaction = {
         'sender': sender,
@@ -44,20 +52,35 @@ def new_transaction(request: Request, sender: str = Form(...), recipient: str = 
 
 @app.get('/new_transaction')
 def get_new_transaction(request: Request):
+    """New transaction route
+    """
+
     return templates.TemplateResponse('new_transaction.html', {'request': request})
 
 
 @app.get('/mine', response_class=HTMLResponse)
 def mine(request: Request):
+    """Mining route (mining happens when routing)
+    """
+
     ret = blockchain.mine()
 
+    last_index = blockchain.last_block.index
+
     if not ret:
-        ret = 'Nothing to mine.'
-    return templates.TemplateResponse('mine.html', {'request': request, 'new_block_index': ret})
+        ret = 'No blocks mined :('
+    else:
+        ret = 'Blocks mined! :D'
+
+    return templates.TemplateResponse('mine.html',
+                                      {'request': request, 'nr_of_mined': ret, 'index_of_last': last_index})
 
 
 @app.get('/')
 def home(request: Request):
+    """Home page route
+    """
+
     return templates.TemplateResponse('home.html', {'request': request})
 
 
